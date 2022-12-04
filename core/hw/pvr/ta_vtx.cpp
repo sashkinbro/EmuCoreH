sashkinbro/EmuCoreH

commit 5a9a52a7dbd8ea2c96fa329e132f32ce3ac565d8

naomi2: remove geom shader. Merge transparent polys. bump mapping fix.
elan: Get rid of geometry shader and do geometry clipping manually.
pvr: Aggressively merge transparent polys in per-triangl and OIT mode, duplicating the last vertex to merge opposite cull mode strips.
gl4: reset OIT pointers in final shader. No need for clear pass.
gl4: alternate between 2 buffers for verts.index/tr poly params.
gl4: use gl_VertexID + poly number for poly index.
gl: new GlBuffer class.

core/hw/pvr/ta_vtx.cpp
@@ -828,11 +828,11 @@ private:
 	template<class T>
 	static Vertex* vert_cvt_base_(T* vtx)
 	{
-		f32 invW=vtx->xyz[2];
-		Vertex* cv=vd_rc.verts.Append();
-		cv->x=vtx->xyz[0];
-		cv->y=vtx->xyz[1];
-		cv->z=invW;
+		f32 invW = vtx->xyz[2];
+		Vertex* cv = vd_rc.verts.Append();
+		cv->x = vtx->xyz[0];
+		cv->y = vtx->xyz[1];
+		cv->z = invW;
 		update_fz(invW);
 		return cv;
 	}
@@ -1410,34 +1410,25 @@ static void make_index(const List<PolyParam> *polys, int first, int end, bool me
 		bool dupe_next_vtx = false;
 		if (merge
 				&& last_poly != nullptr
-				&& last_poly->count != 0)
+				&& last_poly->count != 0
+				&& poly->equivalentIgnoreCullingDirection(*last_poly))
 		{
-			if (poly->equivalent(*last_poly))
+			const u32 last_vtx = indices[last_poly->first + last_poly->count - 1];
+			*ctx->idx.Append() = last_vtx;
+			if (poly->isp.CullMode < 2 || poly->isp.CullMode == last_poly->isp.CullMode)
 			{
-				const u32 last_vtx = indices[last_poly->first + last_poly->count - 1];
-				*ctx->idx.Append() = last_vtx;
 				if (cullingReversed)
 					*ctx->idx.Append() = last_vtx;
-				dupe_next_vtx = true;
-				first_index = last_poly->first;
 				cullingReversed = false;
-			}
-			else if (poly->equivalentInverseCull(*last_poly))
-			{
-				const u32 last_vtx = indices[last_poly->first + last_poly->count - 1];
-				*ctx->idx.Append() = last_vtx;
-				if (!cullingReversed)
-					*ctx->idx.Append() = last_vtx;
-				dupe_next_vtx = true;
-				first_index = last_poly->first;
-				cullingReversed = true;
 			}
 			else
 			{
-				last_poly = poly;
-				first_index = ctx->idx.used();
-				cullingReversed = false;
+				if (!cullingReversed)
+					*ctx->idx.Append() = last_vtx;
+				cullingReversed = true;
 			}
+			dupe_next_vtx = true;
+			first_index = last_poly->first;
 		}
 		else
 		{
@@ -1552,7 +1543,6 @@ bool ta_parse_vdrc(TA_context* ctx)
 		TAParserDX.vdec_init();
 	else
 		TAParser.vdec_init();
-	vd_rc.Clear();
 
 	bool empty_context = true;
 	int op_poly_count = 0;
@@ -1571,6 +1561,8 @@ bool ta_parse_vdrc(TA_context* ctx)
 		bgpp->envMapping[1] = false;
 	}
 
+	const bool mergeTranslucent = !config::PerStripSorting || config::RendererType == RenderType::OpenGL_OIT;
+			// TODO || config::RendererType == RenderType::Vulkan_OIT || config::RendererType == RenderType::DirectX11_OIT
 	TA_context *childCtx = ctx;
 	while (childCtx != nullptr)
 	{
@@ -1607,7 +1599,7 @@ bool ta_parse_vdrc(TA_context* ctx)
 			pt_poly_count = render_pass->pt_count;
 			render_pass->tr_count = vd_rc.global_param_tr.used();
 			make_index(&vd_rc.global_param_tr, tr_poly_count,
-					render_pass->tr_count, false, &vd_rc);
+					render_pass->tr_count, mergeTranslucent, &vd_rc);
 			tr_poly_count = render_pass->tr_count;
 			render_pass->mvo_tr_count = vd_rc.global_param_mvo_tr.used();
 			render_pass->autosort = UsingAutoSort(pass);
@@ -1688,11 +1680,13 @@ bool ta_parse_naomi2(TA_context* ctx)
 		int op_count = 0;
 		int pt_count = 0;
 		int tr_count = 0;
+		const bool mergeTranslucent = !config::PerStripSorting || config::RendererType == RenderType::OpenGL_OIT;
+				// TODO || config::RendererType == RenderType::Vulkan_OIT || config::RendererType == RenderType::DirectX11_OIT;
 		for (const RenderPass& pass : ctx->rend.render_passes)
 		{
 			make_index(&ctx->rend.global_param_op, op_count, pass.op_count, true, &ctx->rend);
 			make_index(&ctx->rend.global_param_pt, pt_count, pass.pt_count, true, &ctx->rend);
-			make_index(&ctx->rend.global_param_tr, tr_count, pass.tr_count, false, &ctx->rend);
+			make_index(&ctx->rend.global_param_tr, tr_count, pass.tr_count, mergeTranslucent, &ctx->rend);
 			op_count = pass.op_count;
 			pt_count = pass.pt_count;
 			tr_count = pass.tr_count;

core/rend/gles/gldraw.cpp
@@ -282,7 +282,6 @@ static void SortTriangles(int first, int count)
 	if (!pidx_sort.empty())
 	{
 		//Bind and upload sorted index buffer
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gl.vbo.idxs2); glCheck();
 		if (gl.index_type == GL_UNSIGNED_SHORT)
 		{
 			static bool overrun;
@@ -292,10 +291,10 @@ static void SortTriangles(int first, int count)
 			short_vidx.Init(vidx_sort.size(), &overrun, NULL);
 			for (size_t i = 0; i < vidx_sort.size(); i++)
 				*(short_vidx.Append()) = vidx_sort[i];
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, short_vidx.bytes(), short_vidx.head(), GL_STREAM_DRAW);
+			gl.vbo.idxs2->update(short_vidx.head(), short_vidx.bytes());
 		}
 		else
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, vidx_sort.size() * sizeof(u32), &vidx_sort[0], GL_STREAM_DRAW);
+			gl.vbo.idxs2->update(&vidx_sort[0], vidx_sort.size() * sizeof(u32));
 		glCheck();
 	}
 }
@@ -374,7 +373,7 @@ void DrawSorted(bool multipass)
 			}
 		}
 		// Re-bind the previous index buffer for subsequent render passes
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gl.vbo.idxs);
+		gl.vbo.idxs->bind();
 	}
 }
 
@@ -481,8 +480,8 @@ void SetupMainVBO()
 	if (gl.vbo.mainVAO != 0)
 	{
 		glBindVertexArray(gl.vbo.mainVAO);
-		glBindBuffer(GL_ARRAY_BUFFER, gl.vbo.geometry);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gl.vbo.idxs);
+		gl.vbo.geometry->bind();
+		gl.vbo.idxs->bind();
 		return;
 	}
 	if (gl.gl_major >= 3)
@@ -491,8 +490,8 @@ void SetupMainVBO()
 		glBindVertexArray(gl.vbo.mainVAO);
 	}
 #endif
-	glBindBuffer(GL_ARRAY_BUFFER, gl.vbo.geometry);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gl.vbo.idxs);
+	gl.vbo.geometry->bind();
+	gl.vbo.idxs->bind();
 
 	//setup vertex buffers attrib pointers
 	glEnableVertexAttribArray(VERTEX_POS_ARRAY);
@@ -519,8 +518,7 @@ static void SetupModvolVBO()
 	if (gl.vbo.modvolVAO != 0)
 	{
 		glBindVertexArray(gl.vbo.modvolVAO);
-		glBindBuffer(GL_ARRAY_BUFFER, gl.vbo.modvols);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		gl.vbo.modvols->bind();
 		return;
 	}
 	if (gl.gl_major >= 3)
@@ -529,8 +527,7 @@ static void SetupModvolVBO()
 		glBindVertexArray(gl.vbo.modvolVAO);
 	}
 #endif
-	glBindBuffer(GL_ARRAY_BUFFER, gl.vbo.modvols); glCheck();
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	gl.vbo.modvols->bind();
 
 	//setup vertex buffers attrib pointers
 	glEnableVertexAttribArray(VERTEX_POS_ARRAY);
@@ -755,6 +752,47 @@ bool render_output_framebuffer()
 
 GLuint vmuTextureId[4]={0,0,0,0};
 GLuint lightgunTextureId[4]={0,0,0,0};
+static GLuint osdVao;
+static std::unique_ptr<GlBuffer> osdVerts;
+static std::unique_ptr<GlBuffer> osdIndex;
+
+static void setupOsdVao()
+{
+	if (osdVerts == nullptr)
+		osdVerts = std::unique_ptr<GlBuffer>(new GlBuffer(GL_ARRAY_BUFFER));
+	if (osdIndex == nullptr)
+	{
+		osdIndex = std::unique_ptr<GlBuffer>(new GlBuffer(GL_ELEMENT_ARRAY_BUFFER));
+		GLushort indices[] = { 0, 1, 2, 1, 3 };
+		osdIndex->update(indices, sizeof(indices));
+	}
+#ifndef GLES2
+	if (osdVao != 0)
+	{
+		glBindVertexArray(osdVao);
+		osdVerts->bind();
+		osdIndex->bind();
+		return;
+	}
+	if (gl.gl_major >= 3)
+	{
+		glGenVertexArrays(1, &osdVao);
+		glBindVertexArray(osdVao);
+	}
+#endif
+	osdVerts->bind();
+	osdIndex->bind();
+
+	//setup vertex buffers attrib pointers
+	glEnableVertexAttribArray(VERTEX_POS_ARRAY);
+	glVertexAttribPointer(VERTEX_POS_ARRAY, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, x));
+
+	glEnableVertexAttribArray(VERTEX_COL_BASE_ARRAY);
+	glVertexAttribPointer(VERTEX_COL_BASE_ARRAY, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, col));
+
+	glEnableVertexAttribArray(VERTEX_UV_ARRAY);
+	glVertexAttribPointer(VERTEX_UV_ARRAY, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, u));
+}
 
 void UpdateVmuTexture(int vmu_screen_number)
 {
@@ -813,7 +851,9 @@ void DrawVmuTexture(u8 vmu_screen_number)
 	glcache.Enable(GL_BLEND);
 	glcache.BlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	SetupMainVBO();
+	setupOsdVao();
+	osdVerts->bind();
+	osdIndex->bind();
 	PipelineShader *shader = GetProgram(false, false, true, true, false, 0, false, 2, false, false, false, false, false, false);
 	glcache.UseProgram(shader->program);
 
@@ -824,10 +864,7 @@ void DrawVmuTexture(u8 vmu_screen_number)
 				{ x+w, y+h, 1, { 255, 255, 255, 255 }, { 0, 0, 0, 0 }, 1, 0 },
 				{ x+w, y,   1, { 255, 255, 255, 255 }, { 0, 0, 0, 0 }, 1, 1 },
 		};
-		GLushort indices[] = { 0, 1, 2, 1, 3 };
-
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STREAM_DRAW);
+		osdVerts->update(vertices, sizeof(vertices));
 	}
 
 	glDrawElements(GL_TRIANGLE_STRIP, 5, GL_UNSIGNED_SHORT, (void *)0);
@@ -907,7 +944,9 @@ void DrawGunCrosshair(u8 port)
 	glcache.Enable(GL_BLEND);
 	glcache.BlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-	SetupMainVBO();
+	setupOsdVao();
+	osdVerts->bind();
+	osdIndex->bind();
 	PipelineShader *shader = GetProgram(false, false, true, true, false, 0, false, 2, false, false, false, false, false, false);
 	glcache.UseProgram(shader->program);
 
@@ -918,10 +957,7 @@ void DrawGunCrosshair(u8 port)
 				{ x+w, y+h, 1, { 255, 255, 255, 255 }, { 0, 0, 0, 0 }, 1, 1 },
 				{ x+w, y,   1, { 255, 255, 255, 255 }, { 0, 0, 0, 0 }, 1, 0 },
 		};
-		GLushort indices[] = { 0, 1, 2, 1, 3 };
-
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STREAM_DRAW);
+		osdVerts->update(vertices, sizeof(vertices));
 	}
 
 	glDrawElements(GL_TRIANGLE_STRIP, 5, GL_UNSIGNED_SHORT, (void *)0);
@@ -935,5 +971,14 @@ void termVmuLightgun()
 	memset(vmuTextureId, 0, sizeof(vmuTextureId));
 	glcache.DeleteTextures(ARRAY_SIZE(lightgunTextureId), lightgunTextureId);
 	memset(lightgunTextureId, 0, sizeof(lightgunTextureId));
+	osdVerts.reset();
+	osdIndex.reset();
+#ifndef GLES2
+	if (gl.gl_major >= 3 && osdVao != 0)
+	{
+		glDeleteVertexArrays(1, &osdVao);
+		osdVao = 0;
+	}
+#endif
 }
 #endif

core/rend/sorter.cpp
@@ -75,43 +75,30 @@ void SortPParams(int first, int count)
 			if (pp->isNaomi2())
 			{
 				glm::mat4 mvMat = pp->mvMatrix != nullptr ? glm::make_mat4(pp->mvMatrix) : glm::mat4(1);
-				glm::mat4 projMat = glm::make_mat4(pp->projMatrix);
-				glm::vec4 min{ 1e38f, 1e38f, 1e38f, 0.f };
-				glm::vec4 max{ -1e38f, -1e38f, -1e38f, 0.f };
+				glm::vec3 min{ 1e38f, 1e38f, 1e38f };
+				glm::vec3 max{ -1e38f, -1e38f, -1e38f };
 				while (vtx != vtx_end)
 				{
-					glm::vec4 pos{ vtx->x, vtx->y, vtx->z, 0.f };
+					glm::vec3 pos{ vtx->x, vtx->y, vtx->z };
 					min = glm::min(min, pos);
 					max = glm::max(max, pos);
 					vtx++;
 				}
-				glm::vec4 center = (min + max) / 2.f;
-				center.w = 1;
-				glm::vec4 extents = max - center;
+				glm::vec4 center((min + max) / 2.f, 1);
+				glm::vec4 extents(max - glm::vec3(center), 0);
 				// transform
 				center = mvMat * center;
 				glm::vec3 extentX = mvMat * glm::vec4(extents.x, 0, 0, 0);
 				glm::vec3 extentY = mvMat * glm::vec4(0, extents.y, 0, 0);
 				glm::vec3 extentZ = mvMat * glm::vec4(0, 0, extents.z, 0);
 				// new AA extents
-				const float newX = std::abs(glm::dot(glm::vec3{ 1.f, 0.f, 0.f }, extentX)) +
-						std::abs(glm::dot(glm::vec3{ 1.f, 0.f, 0.f }, extentY)) +
-						std::abs(glm::dot(glm::vec3{ 1.f, 0.f, 0.f }, extentZ));
+				glm::vec3 newExtent = glm::abs(extentX) + glm::abs(extentY) + glm::abs(extentZ);
 
-				const float newY = std::abs(glm::dot(glm::vec3{ 0.f, 1.f, 0.f }, extentX)) +
-						std::abs(glm::dot(glm::vec3{ 0.f, 1.f, 0.f }, extentY)) +
-						std::abs(glm::dot(glm::vec3{ 0.f, 1.f, 0.f }, extentZ));
+				min = glm::vec3(center) - newExtent;
+				max = glm::vec3(center) + newExtent;
 
-				const float newZ = std::abs(glm::dot(glm::vec3{ 0.f, 0.f, 1.f }, extentX)) +
-						std::abs(glm::dot(glm::vec3{ 0.f, 0.f, 1.f }, extentY)) +
-						std::abs(glm::dot(glm::vec3{ 0.f, 0.f, 1.f }, extentZ));
-				min = center - glm::vec4(newX, newY, newZ, 0);
-				max = center + glm::vec4(newX, newY, newZ, 0);
 				// project
-				glm::vec4 a = projMat * min;
-				glm::vec4 b = projMat * max;
-
-				pp->zvZ = 1 / std::max(a.w, b.w);
+				pp->zvZ = -1 / std::min(min.z, max.z);
 			}
 			else
 			{
@@ -247,8 +234,13 @@ void GenSorted(int first, int count, std::vector<SortTrigDrawParam>& pidx_sort, 
 	//Merge pids/draw cmds if two different pids are actually equal
 	for (u32 k = 1; k < aused; k++)
 		if (lst[k].pid != lst[k - 1].pid)
-			if (pp_base[lst[k].pid].equivalent(pp_base[lst[k - 1].pid]))
+		{
+			const PolyParam& curPoly = pp_base[lst[k].pid];
+			const PolyParam& prevPoly = pp_base[lst[k - 1].pid];
+			if (curPoly.equivalentIgnoreCullingDirection(prevPoly)
+					&& (curPoly.isp.CullMode < 2 || curPoly.isp.CullMode == prevPoly.isp.CullMode))
 				lst[k].pid = lst[k - 1].pid;
+		}
 
 	//re-assemble them into drawing commands
 	vidx_sort.resize(aused*3);

commit 0702dddefc1a68d46c68159f700fae515f66b68b

pvr: don't merge polys in per-triangle. merge polys in per-strip
Merging polygons in per-triangle can produces a large number of
degenerate triangles (up to x3), which must then be sorted, resulting in
a performance hit. This is useless since the sorted triangles are merged
after being sorted. (Regression introduced by
5a9a52a7dbd8ea2c96fa329e132f32ce3ac565d8)
Issue #818

In per-strip mode, polygons are now sorted before creating the index and
merging. So it doesn't have to be done in the renderer anymore.

core/hw/pvr/ta_vtx.cpp
@@ -12,6 +12,8 @@
 
 #include <algorithm>
 #include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #define TACALL DYNACALL
 #ifdef NDEBUG
@@ -1274,6 +1276,78 @@ static void fix_texture_bleeding(const List<PolyParam> *list)
 	}
 }
 
+static bool operator<(const PolyParam& left, const PolyParam& right)
+{
+	return left.zvZ < right.zvZ;
+}
+
+static void sortPolyParams(List<PolyParam> *polys, int first, int end, rend_context* ctx)
+{
+	if (end - first <= 1)
+		return;
+
+	Vertex *vtx_base = ctx->verts.head();
+
+	PolyParam *pp = &polys->head()[first];
+	PolyParam *pp_end = &polys->head()[end];
+
+	while (pp != pp_end)
+	{
+		if (pp->count < 3)
+		{
+			pp->zvZ = 0;
+		}
+		else
+		{
+			Vertex *vtx = &vtx_base[pp->first];
+			Vertex *vtx_end = &vtx_base[pp->first + pp->count];
+
+			if (pp->isNaomi2())
+			{
+				glm::mat4 mvMat = pp->mvMatrix != nullptr ? glm::make_mat4(pp->mvMatrix) : glm::mat4(1);
+				glm::vec3 min{ 1e38f, 1e38f, 1e38f };
+				glm::vec3 max{ -1e38f, -1e38f, -1e38f };
+				while (vtx != vtx_end)
+				{
+					glm::vec3 pos{ vtx->x, vtx->y, vtx->z };
+					min = glm::min(min, pos);
+					max = glm::max(max, pos);
+					vtx++;
+				}
+				glm::vec4 center((min + max) / 2.f, 1);
+				glm::vec4 extents(max - glm::vec3(center), 0);
+				// transform
+				center = mvMat * center;
+				glm::vec3 extentX = mvMat * glm::vec4(extents.x, 0, 0, 0);
+				glm::vec3 extentY = mvMat * glm::vec4(0, extents.y, 0, 0);
+				glm::vec3 extentZ = mvMat * glm::vec4(0, 0, extents.z, 0);
+				// new AA extents
+				glm::vec3 newExtent = glm::abs(extentX) + glm::abs(extentY) + glm::abs(extentZ);
+
+				min = glm::vec3(center) - newExtent;
+				max = glm::vec3(center) + newExtent;
+
+				// project
+				pp->zvZ = -1 / std::min(min.z, max.z);
+			}
+			else
+			{
+				u32 zv = 0xFFFFFFFF;
+				while (vtx != vtx_end)
+				{
+					zv = std::min(zv, (u32&)vtx->z);
+					vtx++;
+				}
+
+				pp->zvZ = (f32&)zv;
+			}
+		}
+		pp++;
+	}
+
+	std::stable_sort(&polys->head()[first], pp_end);
+}
+
 static bool ta_parse_vdrc(TA_context* ctx)
 {
 	ctx->rend_inuse.lock();
@@ -1295,10 +1369,11 @@ static bool ta_parse_vdrc(TA_context* ctx)
 		empty_context = false;
 	}
 
-	const bool mergeTranslucent = !config::PerStripSorting
-			|| config::RendererType == RenderType::OpenGL_OIT
+	const bool perPixel = config::RendererType == RenderType::OpenGL_OIT
 			|| config::RendererType == RenderType::DirectX11_OIT
 			|| config::RendererType == RenderType::Vulkan_OIT;
+	const bool mergeTranslucent = config::PerStripSorting || perPixel;
+
 	TA_context *childCtx = ctx;
 	int pass = 0;
 	while (childCtx != nullptr)
@@ -1328,6 +1403,7 @@ static bool ta_parse_vdrc(TA_context* ctx)
 		if (pass == 0 || !empty_pass)
 		{
 			RenderPass *render_pass = vd_rc.render_passes.Append();
+			getRegionSettings(pass, *render_pass);
 			render_pass->op_count = vd_rc.global_param_op.used();
 			make_index(&vd_rc.global_param_op, op_poly_count,
 					render_pass->op_count, true, &vd_rc);
@@ -1338,11 +1414,13 @@ static bool ta_parse_vdrc(TA_context* ctx)
 					render_pass->pt_count, true, &vd_rc);
 			pt_poly_count = render_pass->pt_count;
 			render_pass->tr_count = vd_rc.global_param_tr.used();
+			if (render_pass->autosort && config::PerStripSorting && !perPixel)
+				sortPolyParams(&vd_rc.global_param_tr, tr_poly_count,
+					render_pass->tr_count, &vd_rc);
 			make_index(&vd_rc.global_param_tr, tr_poly_count,
 					render_pass->tr_count, mergeTranslucent, &vd_rc);
 			tr_poly_count = render_pass->tr_count;
 			render_pass->mvo_tr_count = vd_rc.global_param_mvo_tr.used();
-			getRegionSettings(pass, *render_pass);
 		}
 		childCtx = childCtx->nextContext;
 		pass++;
@@ -1413,14 +1491,16 @@ static bool ta_parse_naomi2(TA_context* ctx)
 		int op_count = 0;
 		int pt_count = 0;
 		int tr_count = 0;
-		const bool mergeTranslucent = !config::PerStripSorting
-				|| config::RendererType == RenderType::OpenGL_OIT
+		const bool perPixel = config::RendererType == RenderType::OpenGL_OIT
 				|| config::RendererType == RenderType::DirectX11_OIT
 				|| config::RendererType == RenderType::Vulkan_OIT;
+		const bool mergeTranslucent = config::PerStripSorting || perPixel;
 		for (const RenderPass& pass : ctx->rend.render_passes)
 		{
 			make_index(&ctx->rend.global_param_op, op_count, pass.op_count, true, &ctx->rend);
 			make_index(&ctx->rend.global_param_pt, pt_count, pass.pt_count, true, &ctx->rend);
+			if (pass.autosort && config::PerStripSorting && !perPixel)
+				sortPolyParams(&ctx->rend.global_param_tr, tr_count, pass.tr_count, &ctx->rend);
 			make_index(&ctx->rend.global_param_tr, tr_count, pass.tr_count, mergeTranslucent, &ctx->rend);
 			op_count = pass.op_count;
 			pt_count = pass.pt_count;

core/rend/dx11/dx11_renderer.cpp
@@ -882,7 +882,6 @@ void DX11Renderer::drawStrips()
 			}
 			else
 			{
-				SortPParams(previous_pass.tr_count, tr_count);
 				drawList<ListType_Translucent, true>(pvrrc.global_param_tr, previous_pass.tr_count, tr_count);
 			}
 		}

core/rend/dx9/d3d_renderer.cpp
@@ -775,7 +775,6 @@ void D3DRenderer::drawStrips()
 			}
 			else
 			{
-				SortPParams(previous_pass.tr_count, tr_count);
 				drawList<ListType_Translucent, true>(pvrrc.global_param_tr, previous_pass.tr_count, tr_count);
 			}
 		}

core/rend/gles/gldraw.cpp
@@ -688,7 +688,6 @@ void DrawStrips()
 				}
 				else
 				{
-					SortPParams(previous_pass.tr_count, current_pass.tr_count - previous_pass.tr_count);
 					DrawList<ListType_Translucent,true>(pvrrc.global_param_tr, previous_pass.tr_count, current_pass.tr_count - previous_pass.tr_count);
 				}
             }

core/rend/sorter.cpp
@@ -17,8 +17,6 @@
 #include "sorter.h"
 #include "hw/pvr/Renderer_if.h"
 #include <algorithm>
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
 struct IndexTrig
 {
@@ -37,85 +35,10 @@ static bool operator<(const IndexTrig& left, const IndexTrig& right)
 	return left.z < right.z;
 }
 
-static bool operator<(const PolyParam& left, const PolyParam& right)
-{
-	return left.zvZ < right.zvZ;
-}
-
 static float getProjectedZ(const Vertex *v, const float *mat)
 {
 	// -1 / z
 	return -1 / (mat[2] * v->x + mat[1 * 4 + 2] * v->y + mat[2 * 4 + 2] * v->z + mat[3 * 4 + 2]);
-}
-
-void SortPParams(int first, int count)
-{
-	if (pvrrc.verts.used() == 0 || count <= 1)
-		return;
-
-	Vertex* vtx_base=pvrrc.verts.head();
-	u32* idx_base = pvrrc.idx.head();
-
-	PolyParam* pp = &pvrrc.global_param_tr.head()[first];
-	PolyParam* pp_end = pp + count;
-
-	while(pp!=pp_end)
-	{
-		if (pp->count<2)
-		{
-			pp->zvZ=0;
-		}
-		else
-		{
-			u32* idx = idx_base + pp->first;
-
-			Vertex* vtx=vtx_base+idx[0];
-			Vertex* vtx_end=vtx_base + idx[pp->count-1]+1;
-
-			if (pp->isNaomi2())
-			{
-				glm::mat4 mvMat = pp->mvMatrix != nullptr ? glm::make_mat4(pp->mvMatrix) : glm::mat4(1);
-				glm::vec3 min{ 1e38f, 1e38f, 1e38f };
-				glm::vec3 max{ -1e38f, -1e38f, -1e38f };
-				while (vtx != vtx_end)
-				{
-					glm::vec3 pos{ vtx->x, vtx->y, vtx->z };
-					min = glm::min(min, pos);
-					max = glm::max(max, pos);
-					vtx++;
-				}
-				glm::vec4 center((min + max) / 2.f, 1);
-				glm::vec4 extents(max - glm::vec3(center), 0);
-				// transform
-				center = mvMat * center;
-				glm::vec3 extentX = mvMat * glm::vec4(extents.x, 0, 0, 0);
-				glm::vec3 extentY = mvMat * glm::vec4(0, extents.y, 0, 0);
-				glm::vec3 extentZ = mvMat * glm::vec4(0, 0, extents.z, 0);
-				// new AA extents
-				glm::vec3 newExtent = glm::abs(extentX) + glm::abs(extentY) + glm::abs(extentZ);
-
-				min = glm::vec3(center) - newExtent;
-				max = glm::vec3(center) + newExtent;
-
-				// project
-				pp->zvZ = -1 / std::min(min.z, max.z);
-			}
-			else
-			{
-				u32 zv=0xFFFFFFFF;
-				while(vtx!=vtx_end)
-				{
-					zv = std::min(zv, (u32&)vtx->z);
-					vtx++;
-				}
-
-				pp->zvZ=(f32&)zv;
-			}
-		}
-		pp++;
-	}
-
-	std::stable_sort(pvrrc.global_param_tr.head() + first, pvrrc.global_param_tr.head() + first + count);
 }
 
 const static Vertex *vtx_sort_base;

core/rend/sorter.h
@@ -20,9 +20,6 @@
 
 #include <vector>
 
-//Sort based on min-z of each strip
-void SortPParams(int first, int count);
-
 struct SortTrigDrawParam
 {
 	const PolyParam* ppid;

core/rend/vulkan/drawer.cpp
@@ -303,17 +303,6 @@ bool Drawer::Draw(const Texture *fogTexture, const Texture *paletteTexture)
 
 	setFirstProvokingVertex(pvrrc);
 
-	// Do per-poly sorting
-	RenderPass previous_pass = {};
-	if (config::PerStripSorting)
-		for (int render_pass = 0; render_pass < pvrrc.render_passes.used(); render_pass++)
-		{
-			const RenderPass& current_pass = pvrrc.render_passes.head()[render_pass];
-			if (current_pass.autosort)
-				SortPParams(previous_pass.tr_count, current_pass.tr_count - previous_pass.tr_count);
-			previous_pass = current_pass;
-		}
-
 	// Upload vertex and index buffers
 	VertexShaderUniforms vtxUniforms;
 	vtxUniforms.ndcMat = matrices.GetNormalMatrix();
@@ -334,7 +323,7 @@ bool Drawer::Draw(const Texture *fogTexture, const Texture *paletteTexture)
 	std::array<float, 5> pushConstants = { 0, 0, 0, 0, 0 };
 	cmdBuffer.pushConstants<float>(pipelineManager->GetPipelineLayout(), vk::ShaderStageFlagBits::eFragment, 0, pushConstants);
 
-	previous_pass = {};
+	RenderPass previous_pass{};
     for (int render_pass = 0; render_pass < pvrrc.render_passes.used(); render_pass++)
     {
         const RenderPass& current_pass = pvrrc.render_passes.head()[render_pass];