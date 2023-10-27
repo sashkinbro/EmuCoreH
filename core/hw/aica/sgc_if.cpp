sashkinbro/EmuCoreH

commit ef3b297b36272e98809f5af66d0e0fe3c4a2a7d3

aica: ignore LEA if <= LSA
Fixes Legacy of Kain Ash Village music
Issue #1104

core/hw/aica/sgc_if.cpp
@@ -586,32 +586,29 @@ struct ChannelEx
 		if (AEG.state != EG_Release)
 			return;
 
-		//if it was off then turn it on !
 		enable();
 
 		// reset AEG
 		SetAegState(EG_Attack);
 		AEG.SetValue(0x280);	// start value taken from HT
 
-		//reset FEG
+		// reset FEG
 		SetFegState(EG_Attack);
-		//set values and crap
 
+		// reset sampling state
+		CA = 0;
+		step.full = 0;
 
-		//Reset sampling state
-		CA=0;
-		step.full=0;
-
-		loop.looped=false;
+		loop.looped = false;
 
 		adpcm.Reset(this);
 
 		StepStreamInitial(this);
-		key_printf("[%d] KEY_ON %s @ %f Hz, loop %d - AEG AR %d DC1R %d DC2V %d DC2R %d RR %d - KRS %d OCT %d FNS %d - PFLOS %d PFLOWS %d",
+		key_printf("[%d] KEY_ON %s @ %f Hz, loop %d - AEG AR %d DC1R %d DC2V %d DC2R %d RR %d - KRS %d OCT %d FNS %d - PFLOS %d PFLOWS %d - SA %x LSA %x LEA %x",
 				ChannelNumber, stream_names[ccd->PCMS], (44100.0 * update_rate) / 1024, ccd->LPCTL,
 				ccd->AR, ccd->D1R, ccd->DL << 5, ccd->D2R, ccd->RR,
 				ccd->KRS, ccd->OCT, ccd->FNS >> 9,
-				ccd->PLFOS, ccd->PLFOWS);
+				ccd->PLFOS, ccd->PLFOWS, (int)(SA - &aica_ram[0]), ccd->LSA, ccd->LEA);
 	}
 
 	void KEY_OFF()
@@ -645,8 +642,12 @@ struct ChannelEx
 	//LSA,LEA
 	void UpdateLoop()
 	{
-		loop.LSA=ccd->LSA;
-		loop.LEA=ccd->LEA;
+		loop.LSA = ccd->LSA;
+		if (ccd->LEA > ccd->LSA)
+			loop.LEA = ccd->LEA;
+		else
+			// Legacy of Kain
+			loop.LEA = 0xffff;
 	}
 
 	s32 EG_BaseRate()

commit bd5f7e09c65a819be60ba5599df4f8efdf1061cf

aica: don't change LEA for non-looping sounds
Non-looping sounds with LSA=0 and LEA=0 shouldn't be played at all.
Regression introduced by ef3b297b36272e98809f5af66d0e0fe3c4a2a7d3.
Fixes white noise in Sonic Adventure Emerald Coast action stage.
Issue #1264

core/hw/aica/sgc_if.cpp
@@ -600,6 +600,9 @@ struct ChannelEx
 		step.full = 0;
 
 		loop.looped = false;
+		if (loop.LEA <= loop.LSA && ccd->LPCTL == 1)
+			// Legacy of Kain
+			loop.LEA = 0xffff;
 
 		adpcm.Reset(this);
 
@@ -607,7 +610,7 @@ struct ChannelEx
 		key_printf("[%d] KEY_ON %s @ %f Hz, loop %d - AEG AR %d DC1R %d DC2V %d DC2R %d RR %d - KRS %d OCT %d FNS %d - PFLOS %d PFLOWS %d - SA %x LSA %x LEA %x",
 				ChannelNumber, stream_names[ccd->PCMS], (44100.0 * update_rate) / 1024, ccd->LPCTL,
 				ccd->AR, ccd->D1R, ccd->DL << 5, ccd->D2R, ccd->RR,
-				ccd->KRS, ccd->OCT, ccd->FNS >> 9,
+				ccd->KRS, ccd->OCT, ccd->FNS,
 				ccd->PLFOS, ccd->PLFOWS, (int)(SA - &aica_ram[0]), ccd->LSA, ccd->LEA);
 	}
 
@@ -643,11 +646,7 @@ struct ChannelEx
 	void UpdateLoop()
 	{
 		loop.LSA = ccd->LSA;
-		if (ccd->LEA > ccd->LSA)
-			loop.LEA = ccd->LEA;
-		else
-			// Legacy of Kain
-			loop.LEA = 0xffff;
+		loop.LEA = ccd->LEA;
 	}
 
 	s32 EG_BaseRate()